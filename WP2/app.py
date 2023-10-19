e0aef63ddfaf25710849fc44f208364ba32d47dd
nilanyK <101003950+nilanyK@users.noreply.github.com>
2023-10-19 18:51:41 +0200

    Update app.py

--- WP2/app.py
+++ WP2/app.py
@@ -1,44 +1,24 @@
-import os
 import streamlit as st
-import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import pandas as pd
 
-# Function to load movie reviews from text files in a folder
-def load_txt_files(folder_path):
-    """Loads all of the txt files in a folder into a list of strings.
+# Load the movie reviews dataset
+from nltk.corpus import movie_reviews
+reviews = [(list(movie_reviews.words(fileid)), category) for category in movie_reviews.categories() for fileid in movie_reviews.fileids(category)]
 
-    Args:
-      folder_path: The path to the folder containing the txt files.
-
-    Returns:
-      A list of strings, where each string contains a movie review.
-    """
-    reviews = []
-    for filename in os.listdir(folder_path):
-        filepath = os.path.join(folder_path, filename)
-        with open(filepath, "r", encoding="utf-8") as f:
-            review = f.read()
-            reviews.append(review)
-    return reviews
-
-
-# Load movie reviews from text files
-neg_reviews = load_txt_files("neg")
-pos_reviews = load_txt_files("pos")
-
-# Combine the positive and negative reviews into a single DataFrame
-reviews_df = pd.DataFrame(data={'review': neg_reviews + pos_reviews, 'sentiment': ['negative'] * len(neg_reviews) + ['positive'] * len(pos_reviews)})
+# Create a DataFrame
+df = pd.DataFrame(reviews, columns=['text', 'sentiment'])
 
 # Create a Bag of Words model
 cv = CountVectorizer(max_features=2000)
-X = cv.fit_transform(reviews_df['review'])
+X = cv.fit_transform(df['text'].apply(' '.join))
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, reviews_df['sentiment'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, df['sentiment'], test_size=0.2, random_state=42)
 
 # Train a Naive Bayes classifier
 nb_classifier = MultinomialNB()
@@ -48,7 +28,6 @@ nb_classifier.fit(X_train, y_train)
 y_pred = nb_classifier.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
-# Streamlit app
 st.title("Movie Review Sentiment Analyzer")
 
 # Input text box for user reviews
@@ -66,3 +45,4 @@ if user_input:
 
     # Display accuracy of the model (optional)
     st.write(f"Model Accuracy: {accuracy:.2f}")
+
