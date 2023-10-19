11447d456ec646a829229fb065bd1bdcd76442fc
nilanyK <101003950+nilanyK@users.noreply.github.com>
2023-10-19 17:39:40 +0200

    Update app.py

--- WP2/app.py
+++ WP2/app.py
@@ -1,114 +1,67 @@
+import os
 import streamlit as st
-import streamlit as st
-import nltk
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-# Download necessary data
-nltk.download("sentiwordnet")
-nltk.download("punkt")
-from nltk.corpus import sentiwordnet as swn
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk import pos_tag
-import nltk
+# Function to load movie reviews from text files in a folder
+def load_txt_files(folder_path):
+    """Loads all of the txt files in a folder into a list of strings.
 
+    Args:
+      folder_path: The path to the folder containing the txt files.
 
-nltk.download('wordnet')
-nltk.download('punkt')
-nltk.download('sentiwordnet')
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
+    Returns:
+      A list of strings, where each string contains a movie review.
+    """
+    reviews = []
+    for filename in os.listdir(folder_path):
+        filepath = os.path.join(folder_path, filename)
+        with open(filepath, "r", encoding="utf-8") as f:
+            review = f.read()
+            reviews.append(review)
+    return reviews
 
-# Function to calculate sentiment scores using SentiWordNet
-def get_sentiment_scores(word):
-    synsets = list(swn.senti_synsets(word))
-    if synsets:
-        sentiment_scores = [(synset.pos_score(), synset.neg_score(), synset.obj_score()) for synset in synsets]
-        return sentiment_scores
-    else:
-        return [(0.0, 0.0, 0.0)]  # Return a single neutral score if no sentiment scores are available
+# Load movie reviews from text files
+neg_reviews = load_txt_files("neg")
+pos_reviews = load_txt_files("pos")
 
-# Function to identify adverbs and adjectives and their sentiment scores
-def analyze_review(review_text):
-    stop_words = set(stopwords.words('english'))
-    words = word_tokenize(review_text)
-    tagged_words = pos_tag(words)
-    adverbs_and_adjectives = [word for word, pos in tagged_words if (pos == 'RB' or pos == 'JJ') and word.lower() not in stop_words]
-    
-    adverb_and_adjective_sentiment_scores = []
-    for word in adverbs_and_adjectives:
-        sentiment_scores = get_sentiment_scores(word)
-        adverb_and_adjective_sentiment_scores.append((word, sentiment_scores))
-    
-    return adverb_and_adjective_sentiment_scores
+# Combine the positive and negative reviews into a single DataFrame
+reviews_df = pd.DataFrame(data={'review': neg_reviews + pos_reviews, 'sentiment': ['negative'] * len(neg_reviews) + ['positive'] * len(pos_reviews)})
 
-# Function to get overall sentiment
+# Create a Bag of Words model
+cv = CountVectorizer(max_features=2000)
+X = cv.fit_transform(reviews_df['review'])
 
-def get_overall_sentiment(sentiment_scores):
-    # Initialize total scores
-    total_positive_score = 0.0
-    total_negative_score = 0.0
-    total_objective_score = 0.0
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, reviews_df['sentiment'], test_size=0.2, random_state=42)
 
-    # Check if the sentiment scores iterable is empty
-    if not sentiment_scores:
-        return "Neutral"
+# Train a Naive Bayes classifier
+nb_classifier = MultinomialNB()
+nb_classifier.fit(X_train, y_train)
 
-    # Iterate over the sentiment scores and sum the positive, negative, and objective scores
-    for score in sentiment_scores:
-        try:
-            positive_score, negative_score, objective_score = score
-            total_positive_score += float(positive_score)
-            total_negative_score += float(negative_score)
-            total_objective_score += float(objective_score)
-        except ValueError:
-            # Ignore scores that cannot be converted to floats
-            pass
+# Evaluate the classifier
+y_pred = nb_classifier.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
 
-    # Check if all scores are greater than or equal to zero
-    if total_positive_score >= 0 and total_negative_score >= 0 and total_objective_score >= 0:
-        # Determine overall sentiment polarity
-        if total_positive_score > total_negative_score and total_positive_score > total_objective_score:
-            return "Positive"
-        elif total_negative_score > total_positive_score and total_negative_score > total_objective_score:
-            return "Negative"
-        elif total_objective_score > total_positive_score and total_objective_score > total_negative_score:
-            return "Objective"
-        else:
-            return "Neutral"
-    else:
-        # Return neutral if any of the scores are less than zero
-        return "Neutral"
+# Streamlit app
+st.title("Movie Review Sentiment Analyzer")
 
+# Input text box for user reviews
+user_input = st.text_area("Enter a movie review:")
 
+if user_input:
+    # Vectorize the user input
+    user_input_vectorized = cv.transform([user_input])
 
-# Streamlit UI
-st.title("Movie Review Sentiment Analysis")
+    # Predict the sentiment
+    sentiment = nb_classifier.predict(user_input_vectorized)
 
-# Input for entering a movie review
-user_input = st.text_area("Enter your movie review:")
-if st.button("Analyze Sentiment"):
-    if user_input:
-        st.subheader("Review:")
-        st.write(user_input)
+    # Display the result
+    st.write("Sentiment:", sentiment[0])
 
-        st.subheader("Adverbs and Adjectives and Their Sentiment Scores:")
-        adverb_and_adjective_sentiment_scores = analyze_review(user_input)
-
-        if not adverb_and_adjective_sentiment_scores:
-            st.write("No adverbs or adjectives found in the review.")
-        else:
-            for word, scores in adverb_and_adjective_sentiment_scores:
-                st.write(f"Word: {word}")
-                for i, score in enumerate(scores):
-                  st.write(f"Score {i + 1}: {score}")
-                st.write("---")
-
-        st.subheader("Overall Sentiment:")
-        overall_sentiment = get_overall_sentiment(adverb_and_adjective_sentiment_scores)
-
-        # Display the overall sentiment, even if the review does not contain any adverbs or adjectives
-        if overall_sentiment is None:
-            st.write("Neutral")
-        else:
-            st.write(overall_sentiment)
-
+    # Display accuracy of the model (optional)
+    st.write(f"Model Accuracy: {accuracy:.2f}")
