d44a996eff73d1405b89dcd71225466d33400f1c
nilanyK <nilany.karunathasan@edu.devinci.fr>
2023-10-22 18:34:02 +0200

    update

--- WP3/app.py
+++ WP3/app.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
+# Suppress the deprecation message
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # Load CNN dataset
@@ -14,6 +15,7 @@ articles, abstracts = cnn.loadCNN()
 
 @st.cache(persist=True)
 def get_tfidf_vectorizer(articles):
+    # Create a TF-IDF vectorizer with specific settings
     tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
     return tfidf_vectorizer.fit(articles)
 
@@ -22,10 +24,18 @@ tfidf_vectorizer = get_tfidf_vectorizer(articles)
 
 @st.cache(persist=True)
 def retrieve_top_documents(query_summary, k=10):
+    # Transform the query summary into a TF-IDF vector
     query_vector = tfidf_vectorizer.transform([query_summary])
+    
+    # Calculate cosine similarity between the query and all articles
     similarity_scores = linear_kernel(query_vector, tfidf_vectorizer.transform(articles))
+    
+    # Sort document indices by similarity score in descending order
     document_indices = similarity_scores[0].argsort()[:-k-1:-1]
+    
+    # Retrieve the top-k documents based on their indices
     top_documents = [articles[i] for i in document_indices]
+    
     return similarity_scores, top_documents  # Return similarity_scores
 
 st.title("📚 CNN Document Retrieval 📚")
@@ -42,5 +52,3 @@ if st.button("🔍 Retrieve Documents"):
         for i, document in enumerate(top_k_documents, start=1):
             st.subheader(f"🏆 Rank {i}:")
             st.write(document)
-
-
